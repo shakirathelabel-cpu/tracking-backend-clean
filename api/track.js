cf6bf649a591f769e902b6ae3fd06d2a448af1fd
shakirathelabel-cpu <shakirathelabel@gmail.com>
2026-02-11 21:01:49 +0530

    track.js

--- api/track.js
+++ api/track.js
@@ -1,7 +1,88 @@
 export default async function handler(req, res) {
 
-  return res.status(200).json({
-    message: "Tracking API is working"
-  });
+  try {
+
+    const { awb } = req.query;
+
+    if (!awb) {
+      return res.status(400).json({ error: "AWB required" });
+    }
+
+    // ---------- TRY DELHIVERY ----------
+    try {
+
+      const delRes = await fetch(
+        `https://track.delhivery.com/api/v1/packages/json/?waybill=${awb}`,
+        {
+          headers: {
+            "Authorization": `Token ${process.env.DEL_TOKEN}`
+          }
+        }
+      );
+
+      const delData = await delRes.json();
+
+      if (delData?.ShipmentData?.length) {
+        return res.json({
+          courier: "Delhivery",
+          data: delData
+        });
+      }
+
+    } catch (e) {
+      console.log("Delhivery error");
+    }
+
+    // ---------- TRY SHIPROCKET ----------
+    try {
+
+      // Login
+      const loginRes = await fetch(
+        "https://apiv2.shiprocket.in/v1/external/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: process.env.SR_EMAIL,
+            password: process.env.SR_PASSWORD
+          })
+        }
+      );
+
+      const loginData = await loginRes.json();
+      const token = loginData.token;
+
+      // Track
+      const srTrack = await fetch(
+        `https://apiv2.shiprocket.in/v1/external/courier/track/awb/${awb}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      const srData = await srTrack.json();
+
+      if (srData?.tracking_data) {
+        return res.json({
+          courier: "Shiprocket",
+          data: srData
+        });
+      }
+
+    } catch (e) {
+      console.log("Shiprocket error");
+    }
+
+    return res.json({
+      status: "Tracking not found"
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      error: "Server error"
+    });
+  }
 
 }
